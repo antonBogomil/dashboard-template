5b33869de4869a732ea8decc21cbe8c073c3d9c0
AntonB <anton.bogomil777@gmail.com>
2019-06-19 18:19:23 +0300

    hooks useForm

--- src/api/api.js
+++ src/api/api.js
@@ -1,23 +1,3 @@
-import {useState, useEffect} from 'react';
-
-export const useFetch = (url, options) => {
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        if (url) {
-            setLoading(true);
-            getData(url, options, (data) => {
-                setData(data);
-                setLoading(false);
-            });
-        } else {
-            setLoading(false);
-            setData([]);
-        }
-    }, []);
-    return [data, loading, error];
-};
 
 export const getData = async function (url, params, onSuccess, onError) {
     // Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
---
+++ src/hook/index.js
@@ -0,0 +1,51 @@
+import {getData} from "../api";
+import {useState, useEffect} from 'react';
+
+export const useFetch = (url, options) => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        if (url) {
+            setLoading(true);
+            getData(url, options, (data) => {
+                setData(data);
+                setLoading(false);
+            });
+        } else {
+            setLoading(false);
+            setData([]);
+        }
+    }, []);
+    return [data, loading, error];
+};
+export const useForm = (callback, validate) => {
+    const [values, setValues] = useState({});
+    const [errors, setErrors] = useState({});
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+
+    useEffect(() => {
+        if (Object.keys(errors).length === 0 && isSubmitting) {
+            callback();
+        }
+    }, [errors]);
+
+
+    const handleSubmit = (event) => {
+        if (event) event.preventDefault();
+        // setErrors(validate(values));
+        setIsSubmitting(true);
+    };
+    const handleChange = (event) => {
+        event.persist();
+        setValues(values => ({ ...values, [event.target.name]: event.target.value }));
+    };
+    return {
+        handleChange,
+        handleSubmit,
+        values,
+        errors,
+    }
+
+};
